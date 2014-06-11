b8117d15e94e95983b16488161237c8b01652e67
binwei lin <binwei92@cs.utexas.edu>
2014-06-11 14:58:37 -0500

    most stupid

--- Collatz.py
+++ Collatz.py
@@ -33,7 +33,21 @@ def collatz_eval (i, j) :
     return the max cycle length in the range [i, j]
     """
     # <your code>
-    return 1
+    max_length = 0
+    for x in range(i, j):
+        n = x
+        length = 1
+        while n != 1:
+            if n%2 == 0:
+                n /= 2
+                length = length + 1
+            else:
+                n = 3*n + 1
+                length = length + 1
+        if length > max_length:
+            max_length = length
+    return max_length
+
 
 # -------------
 # collatz_print
