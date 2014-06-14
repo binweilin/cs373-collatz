2027a5f2c7c4516e10f6252df4c51076bc7798b1
binwei lin <binwei92@cs.utexas.edu>
2014-06-14 15:55:42 -0500

    SphereCollatz.py

---
+++ SphereCollatz.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python3
+
+# ---------------------------
+# projects/collatz/Collatz.py
+# Copyright (C) 2014
+# Glenn P. Downing
+# ---------------------------
+
+# ------------
+# collatz_read
+# ------------
+
+#!/usr/bin/env python3
+
+# ------------------------------
+# projects/collatz/RunCollatz.py
+# Copyright (C) 2014
+# Glenn P. Downing
+# ------------------------------
+
+import sys
+
+cache = {}
+
+def collatz_read (r) :
+    """
+    read two ints
+    r is a reader
+    return a list of the two ints, otherwise a list of zeros
+    """
+    s = r.readline()
+    if s == "" :
+        return []
+    a = s.split()
+    return [int(v) for v in a]
+
+# ------------
+# collatz_eval
+# ------------
+
+def collatz_eval (i, j) :
+    """
+    i is the beginning of the range, inclusive
+    j is the end       of the range, inclusive
+    return the max cycle length in the range [i, j]
+    """
+    # <your code>
+    if i > j:
+        i, j = j , i
+
+    max_length = 1
+    if i < j//2:
+        i = j//2
+
+    for x in range(i, j+1):
+        n = x
+        length = 1
+        if x in cache:
+            length = cache[x]
+        else:
+            while n != 1:               
+                if n%2 == 0:
+                    n //= 2
+                    length = length + 1
+                elif n%2 != 0:
+                    n = (3*n + 1)//2
+                    length = length + 2
+                if n in cache:
+                    length = length + cache[n]-1
+                    n = 1;
+            cache[x] = length
+        if length > max_length:
+            max_length = length
+    return max_length
+
+
+# -------------
+# collatz_print
+# -------------
+
+def collatz_print (w, i, j, v) :
+    """
+    print three ints
+    w is a writer
+    i is the beginning of the range, inclusive
+    j is the end       of the range, inclusive
+    v is the max cycle length
+    """
+    w.write(str(i) + " " + str(j) + " " + str(v) + "\n")
+
+# -------------
+# collatz_solve
+# -------------
+
+def collatz_solve (r, w) :
+    """
+    read, eval, print loop
+    r is a reader
+    w is a writer
+    """
+    while True :
+        a = collatz_read(r)
+        if not a :
+            return
+        i, j = a
+        v = collatz_eval(i, j)
+        collatz_print(w, i, j, v)
+
+collatz_solve(sys.stdin, sys.stdout)
